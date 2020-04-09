f506fd7d8667d7485daf8e0c7f1ec5416c7d2798
Álefe Cruz <alefecrz@gmail.com>
2020-04-09 04:49:29 -0300

    modificação de estrutura.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
-
-import '../../global.css';
 import './style.css';
 
-import Navbar from '../../components/Navbar';
-import logoImg from '../../assets/img/logo.svg';
+export default function Home () {
+    return (
+        <div className="home-conteiner">
 
-export default function Home() {
-  return (
-    <>
-      <Navbar />  
-      <div className="home-conteiner">
-        <div className="content">
-          <div className="logo">
-            <img alt="" src={ logoImg }/>
-          </div>
-          <div className="text">
-            <h1>Álefe Cruz</h1>
-            <h5> &#60;Desenvolvedor/&#62;  </h5>
-          </div>
         </div>
-      </div>
-    </>
-);
+    );
 }
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 
-/*import Login from './pages/Login';
-import Register from './pages/Register'; 
-import Profile from './pages/Profile';
-import NewIncident from './pages/NewIncident';
-*/
 export default function Routes() {
   return ( 
     <BrowserRouter>
