fdd138bd3658116437016698030f9f1979650043
Álefe Cruz <alefecrz@gmail.com>
2020-04-10 18:38:39 -0300

    Create Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import './style.css';
 
+import logoImg from '../../assets/img/logo.svg'
+
+import githubIcon from '../../assets/icons/icon-github.svg'
+import facebookIcon from '../../assets/icons/icon-facebook.svg'
+import instagramIcon from '../../assets/icons/icon-instagram.svg'
+import linkedinIcon from '../../assets/icons/icon-linkedin.svg'
+import mauseIcon from '../../assets/icons/icon-mause.svg'
+
 class Home extends Component {
     render() {
         return (
             <div className= "home-conteiner">
-                <h1>
-                    Home
-                </h1>
+                <img src={ logoImg } alt=""/>
+                <div className="text-conteiner">
+                    <h1>HI! I'M  <span>ÁLEFE CRUZ</span></h1>
+                    <p>I AM <span>FULL STACK</span> DEVELOPER</p>
+                    <button className="button" type="submit">
+                        Ver Projetos
+                    </button>
+                </div>
+                <div className="icons-conteiner">
+                    <img src={ githubIcon } alt=""/>
+                    <img src={ linkedinIcon } alt=""/>
+                    <img src={ facebookIcon } alt=""/>
+                    <img src={ instagramIcon } alt=""/>
+                </div>
+                <div className="down-conteiner">
+                    <img src={ mauseIcon } alt=""/>
+                    <p>SCROLL DOWN</p>
+                </div>
             </div>
         );
     }
--- src/pages/Site/index.js
+++ src/pages/Site/index.js
@@ -3,14 +3,14 @@ import './style.css';
 
 import Home from '../../components/Home';
 import Aboutme from '../../components/Aboutme';
-import Navbar from '../../components/Navbar';
+// import Navbar from '../../components/Navbar';
 import Project from '../../components/Project';
 import Contact from '../../components/Contact';
 
 export default function Site () {
     return (
         <div className="conteiner">
-            <Navbar />
+            {/* <Navbar /> */}
             <Home />
             <Aboutme />
             <Project />
