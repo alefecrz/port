da09ce388dcb9cc133eabf6f2d79d39ee0a21515
Alefe Cruz <alefecrz@gmail.com>
2021-01-11 06:58:52 -0300

    Update aboutme to styled component

--- src/pages/Aboutme/index.js
+++ src/pages/Aboutme/index.js
@@ -1,113 +1,36 @@
-import React, { Component  } from 'react';
-import './style.css';
+import React from 'react';
+import { Conteiner, Text, Image } from './style';
 
 import profileImg from '../../assets/img/img-profile.png'
 
-import HexaBg from '../../assets/icons/icon-hexaBg';
+const Aboutme = () => (
+  <Conteiner>
+    <Image>
+      <img src={ profileImg } alt="" />
+    </Image>
+    <Text>
+      <h1>Sobre</h1>
+      <p>
+        Sempre adorei tecnologia, meu primeiro contato foi aos 10 anos quando ganhei um computador antigo com Windows 95 foi por ele e uma internet discada onde a descoberta desse mundo digital aconteceu.
+      </p>
+      <p>
+        Hoje estou me graduando em Engenharia da Computação na Escola Politécnica de Pernambuco.
+      </p>
+      <p>
+        Possuo 8 anos de experiência com programação e 3 anos de mercado.
+      </p>
+      <p>
+        Minhas princiais atividades são no desenvolvimento de softwares Backend (API, integrações, conexão com banco de dados), Frontend ( estilização, interfaces WEB e Mobile) e também no design (criação de Layout, prototipação e experiência de usuário).
+      </p>
+      <p>
+        Sou apaixonado pelo o que faço e os desafios me motivam todos os dias.
+      </p>
+      <p>
+        Acredito que compartilhar conhecimento e ter boa comunicação são fundamentais para alcançar soluções de sucesso.
+      </p>
+    </Text>
 
-import FireIcon from '../../assets/icons/icon-fire';
-import DbIcon from '../../assets/icons/icon-db';
-import WebIcon from '../../assets/icons/icon-web';
-
-import iconOk from '../../assets/icons/icon-ok.svg';
-
-
-class Aboutme extends Component {
-    render() {
-        return (
-            <div className= "aboutme">
-                <div className="text">
-                    <h1>ABOUT ME</h1>
-                    <p>I'm Computer Engineering Student from Universidade de Pernambuco(UPE). Learning lover, I like to research, learn and discuss new things.
-                    </p>
-                </div>
-                <div className="img">
-                    <img src={ profileImg } alt="" />
-                </div>
-                <div className="skills">
-                    <ul>
-                        <li>
-                            <div className="icon-content"> 
-                                <HexaBg width={100} fill='#2D132C' className="bg"/>
-                                <DbIcon className="icon" />
-                            </div>
-                            <h2>Backend</h2>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>JavaScript</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>Nodejs/Expressjs</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>ApiRest</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>SQL</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>MongoDB</h3>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="icon-content"> 
-                                <HexaBg width={100} fill='#C72C41' className="bg"/>
-                                <WebIcon className="icon" />
-                            </div>
-                            <h2>Frontend</h2>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>HTML/CSS</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>AngularJS</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>Reactjs</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>ReactNative</h3>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="icon-content"> 
-                                <HexaBg width={100} fill='#EE4540' className="bg"/>
-                                <FireIcon className="icon" />
-                            </div>
-                            <h2>Others</h2>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>Prototyping</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>UX</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>Adobe XD</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>Illustration</h3>
-                            </div>
-                            <div className="item">
-                                <img src={iconOk} alt=""/>
-                                <h3>PowerBI</h3>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
-}
+  </Conteiner>
+);
 
 export default Aboutme;
