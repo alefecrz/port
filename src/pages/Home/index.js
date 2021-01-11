04fc4bb39312001f2b9d5afdca1c0de5c427a68e
Alefe Cruz <alefecrz@gmail.com>
2021-01-11 06:59:39 -0300

    Change class component to function component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Conteiner, Content, Text, Icons, Down } from './style';
 
 import SocialIcon from '../../components/SocialIcon';
@@ -11,56 +11,51 @@ import instagramIcon from '../../assets/icons/icon-instagram.svg'
 import linkedinIcon from '../../assets/icons/icon-linkedin.svg'
 import mauseIcon from '../../assets/icons/icon-mause.svg'
 
-class Home extends Component {
-  render() {
-    return (
-      <Conteiner>
-        <Content>
-          <div>
-            <img src={logoImg} alt="" />
-          </div>
-          <Text>
-
-            <h1>HI! I'M  <span>ÁLEFE CRUZ</span></h1>
-            <p>I AM <span>FULL STACK</span> DEVELOPER</p>
-            <button className="button" type="submit">
-              Ver Projetos
-            </button>
-          </Text>
-          <Icons>
-            <SocialIcon
-              target="_blanc"
-              icon={githubIcon}
-              alt="GitHub"
-              href="https://github.com/alefecrz"
-            />
-            <SocialIcon
-              target="_blanc"
-              icon={linkedinIcon}
-              alt="LinkedIN"
-              href="https://www.linkedin.com/in/%C3%A1lefe-cruz-27a64918a/"
-            />
-            <SocialIcon
-              target="_blanc"
-              icon={facebookIcon}
-              alt="Facebook"
-              href="https://www.facebook.com/alefe.cruz"
-            />
-             <SocialIcon
-              target="_blanc"
-              icon={instagramIcon}
-              alt="Instagram"
-              href="https://www.instagram.com/alefecrz/"
-            />
-          </Icons>
-          <Down>
-            <img src={mauseIcon} alt="" />
-            <p>SCROLL DOWN</p>
-          </Down>
-        </Content>
-      </Conteiner>
-    );
-  }
-}
+const Home = () => (
+  <Conteiner>
+    <Content>
+      <div>
+        <img src={logoImg} alt="" />
+      </div>
+      <Text>
+        <h1>HI! I'M  <span>ÁLEFE CRUZ</span></h1>
+        <p>I AM <span>FULL STACK</span> DEVELOPER</p>
+        <button className="button" type="submit">
+          Ver Projetos
+        </button>
+      </Text>
+      <Icons>
+        <SocialIcon
+          target="_blanc"
+          icon={githubIcon}
+          alt="GitHub"
+          href="https://github.com/alefecrz"
+        />
+        <SocialIcon
+          target="_blanc"
+          icon={linkedinIcon}
+          alt="LinkedIN"
+          href="https://www.linkedin.com/in/%C3%A1lefe-cruz-27a64918a/"
+        />
+        <SocialIcon
+          target="_blanc"
+          icon={facebookIcon}
+          alt="Facebook"
+          href="https://www.facebook.com/alefe.cruz"
+        />
+          <SocialIcon
+          target="_blanc"
+          icon={instagramIcon}
+          alt="Instagram"
+          href="https://www.instagram.com/alefecrz/"
+        />
+      </Icons>
+      <Down>
+        <img src={mauseIcon} alt="" />
+        <p>SCROLL DOWN</p>
+      </Down>
+    </Content>
+  </Conteiner>
+);
 
 export default Home;
