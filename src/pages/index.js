cb3531cc561b332dda4d8c73911611c96114da2b
Alefe Cruz <alefecrz@gmail.com>
2021-01-11 09:15:10 -0300

    Add route skill

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 import Home from './Home';
-// import Aboutme from '../../components/Aboutme';
+import Aboutme from './Aboutme';
+import Skill from './Skill';
+// import Contact from './Contact';
 // import Navbar from '../../components/Navbar';
 // import Project from '../../components/Project';
-// import Contact from '../../components/Contact';
 
 export default function Site () {
     return (
         <>
             {/* <Navbar /> */}
             <Home />
-            {/* <Aboutme /> */}
-            {/* <Project />
-            <Contact /> */}
+            <Aboutme />
+            <Skill />
+            {/* <Project />*/}
+            {/* <Contact /> */}
         </>
     );
 }
