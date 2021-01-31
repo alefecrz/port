807a63840eb7f60e5a8527eaac2395b1b59d30b7
Alefe Cruz <alefecrz@gmail.com>
2021-01-31 18:05:06 -0300

    Add height auto on screen

--- src/pages/Aboutme/style.js
+++ src/pages/Aboutme/style.js
@@ -2,12 +2,16 @@ import styled from 'styled-components';
 
 export const Conteiner = styled.div`
   width: 100%;
+  height: 100vh;
   padding: 0rem 4rem 4rem 4rem ;
   display: grid;
   grid-template-columns: 1fr 1fr;
   background-color: #F9F9F9;
+  justify-content: center;
+  justify-items: center;
 
   @media only screen and (max-width: 768px), only screen and (max-height: 520px){
+      height: auto;
       display: flex;
       padding: 0rem 2rem 2rem 2rem ;
       flex-direction: column;
@@ -45,7 +49,6 @@ export const Image = styled.div`
   align-self: center;
 
   img {
-    margin-top: 50px;
     width: 25rem;
   }
 
